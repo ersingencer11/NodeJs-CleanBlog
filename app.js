7ed4b598bbe1b558b0e2faf02bc997ff64bba29f
Ersin Gençer <ersin.gencer11@outlook.com>
2022-07-08 01:15:59 +0300

    ikinci commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
+app.get("/", (req,res)=>{
+    res.send(blog)
+})
+
+const port=3000
+app.listen(port, ()=>{
+    console.log(`${port} portu dinleniyor`)
+})
+
+
+
+
+
+
+/* git push -u origin main*/
